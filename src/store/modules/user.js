31da80aa8b8e65d02f7b70ca5fa73b1da4f5cef3
AJ <ajabad.dev@gmail.com>
2022-01-20 23:29:09 +0800

    Updated user VueX module

--- src/store/modules/user.js
+++ src/store/modules/user.js
@@ -1,25 +1,41 @@
 const store = {
   state: {
-    profile: {
+    user: {
+      userId: null,
+      userStatus: null,
       firstName: null,
       lastName: null,
       mobile: null,
+      profilePhoto: null,
     },
-    userId: null,
-    userStatus: null,
     token: null,
   },
   actions: {
-
+    signIn: ({ getters, commit }, payload) => {
+      commit('setUser', payload.user);
+      commit('setToken', payload.token);
+      localStorage.setItem("token", payload.token);
+      localStorage.setItem("user", JSON.stringify(getters.getUser));
+    },
   },
   mutations: {
-
+    setToken: (state, payload) => {
+      state.token = payload
+    },
+    setUser: (state, payload) => {
+      const { user } = state
+      const { userId, userStatus, firstName, lastName, mobile, profilePhoto } = payload
+      user.userId = userId ?? user.userId;
+      user.userStatus = userStatus ?? user.userStatus;
+      user.firstName = firstName ?? user.firstName;
+      user.lastName = lastName ?? user.lastName;
+      user.mobile = mobile ?? user.mobile;
+      user.profilePhoto = profilePhoto ?? user.profilePhoto;
+    }
   },
   getters: {
-    getProfile: state => state.profile
-    , getUserId: state => state.userId,
-    getUserStatus: state => state.userStatus,
     getToken: state => state.token,
+    getUser: state => state.user,
   }
 }
 
