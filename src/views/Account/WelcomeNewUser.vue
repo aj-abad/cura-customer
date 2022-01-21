094b77dc27d27e28ccc6f2f676fcc8393dd3d4f3
AJ <ajabad.dev@gmail.com>
2022-01-21 23:43:35 +0800

    Added welcome message

--- src/views/Account/WelcomeNewUser.vue
+++ src/views/Account/WelcomeNewUser.vue
@@ -1,5 +1,25 @@
 <template>
-  <div class="primary"></div>
+  <div class="primary d-flex flex-column justify-end">
+    <div class="pa-6 white--text">
+      <div class="text-center pb-6">
+        <h2 class="font-weight-semibold mb-2">Welcome to Cura!</h2>
+        <p class="body-1 mb-6">
+          You're only a few steps away from making self-care easier than ever
+          before.
+        </p>
+        <v-btn
+          v-ripple="{ class: 'primary--text' }"
+          class="py-6"
+          block
+          rounded
+          elevation="0"
+          color="white"
+        >
+          Set up account
+        </v-btn>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
