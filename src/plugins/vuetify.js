38feb25cdb3ffcdf83c2811e9d1699e6c143027e
AJ <ajabad.dev@gmail.com>
2022-01-22 13:53:43 +0800

    Updated vuetify options

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -1,14 +1,13 @@
-import Vue from 'vue'
-import Vuetify from 'vuetify'
-import 'vuetify/dist/vuetify.min.css'
+import Vue from 'vue';
+import Vuetify from 'vuetify/lib/framework';
+import '@mdi/font/css/materialdesignicons.css'
+import "@fontsource/nunito-sans";
+Vue.use(Vuetify);
 
-Vue.use(Vuetify)
-
-const opts = {
+export default new Vuetify({
   theme: {
-    dark: false,
     options: {
-      customProperties: true
+      customProperties: true,
     },
     themes: {
       light: {
@@ -23,20 +22,6 @@ const opts = {
         // accent: '#8c9eff',
         // error: '#b71c1c',
       },
-      dark: {
-        primary: '#0d826e',
-        primarylight: '#00a987',
-        primarylighter: '#aeddd5',
-        secondary: '#f69b7a',
-        secondarylight: '#f7eab5',
-        error: '#820D21',
-        bglight: '#dff2ef'
-        // secondary: '#b0bec5',
-        // accent: '#8c9eff',
-        // error: '#b71c1c',
-      }
-    }
-  }
-}
-
-export default new Vuetify(opts)
+    },
+  },
+});
