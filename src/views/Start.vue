626b16a0b6c1582d56f10831939b640b2fe91c77
AJ <ajabad.dev@gmail.com>
2022-01-22 18:13:46 +0800

    Updated button styles

--- src/views/Start.vue
+++ src/views/Start.vue
@@ -1,18 +1,11 @@
 <template>
   <div class="bglight pt-6">
     <div class="d-flex mb-2 justify-center">
-      <cura-logo
-        style="width: 48px"
-        aria-label="Cura"
-      />
+      <cura-logo style="width: 48px" aria-label="Cura" />
     </div>
     <div class="text-center">
-      <h2 class="font-weight-semibold">
-        Self-care, delivered.
-      </h2>
-      <h3 class="mt-n1 font-weight-medium">
-        Welcome to Cura.
-      </h3>
+      <h2 class="font-weight-semibold">Self-care, delivered.</h2>
+      <h3 class="mt-n1 font-weight-medium">Welcome to Cura.</h3>
     </div>
     <v-sheet
       elevation="6"
@@ -27,30 +20,12 @@
         bottom-sheet
       "
     >
-      <v-btn
-        rounded
-        elevation="0"
-        block
-        color="#3b5998"
-        dark
-        class="py-6 mb-2"
-      >
-        <v-icon class="btn-icon">
-          mdi-facebook
-        </v-icon>
+      <v-btn elevation="0" block large dark color="#3b5998" class="mb-2">
+        <v-icon class="btn-icon"> mdi-facebook </v-icon>
         Continue with Facebook
       </v-btn>
-      <v-btn
-        rounded
-        elevation="0"
-        block
-        color="primary"
-        class="py-6"
-        to="/account/email"
-      >
-        <v-icon class="btn-icon">
-          mdi-email
-        </v-icon>
+      <v-btn elevation="0" block large color="primary" to="/account/email">
+        <v-icon class="btn-icon"> mdi-email </v-icon>
         Continue with email
       </v-btn>
     </v-sheet>
@@ -58,11 +33,11 @@
 </template>
 
 <script>
-import CuraLogo from '../components/SVG/CuraLogo.vue'
+import CuraLogo from "../components/SVG/CuraLogo.vue";
 export default {
   components: { CuraLogo },
-  name: 'Start'
-}
+  name: "Start",
+};
 </script>
 
 <style lang="stylus" scoped>
