f78265cce61610900c5c96ac50108b472838bb82
AJ <ajabad.dev@gmail.com>
2022-01-23 00:42:02 +0800

    Changed text

--- src/views/Account/WelcomeNewUser.vue
+++ src/views/Account/WelcomeNewUser.vue
@@ -1,11 +1,10 @@
 <template>
   <div class="primary d-flex flex-column justify-end">
-    <div class="pa-6 white--text">
-      <div class="text-center pb-6">
+    <div class="pa-8 white--text">
+      <div class="text-center pb-8">
         <h2 class="font-weight-semibold mb-2">Welcome to Cura!</h2>
         <p class="body-1 mb-6">
-          You're only a few steps away from making self-care easier than ever
-          before.
+          You're a few steps away from having self-care at your fingertips.
         </p>
         <v-btn
           v-ripple="{ class: 'primary--text' }"
@@ -14,7 +13,7 @@
           elevation="0"
           color="white"
         >
-          Set up account
+          Proceed
         </v-btn>
       </div>
     </div>
