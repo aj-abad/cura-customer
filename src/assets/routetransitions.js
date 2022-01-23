74ca73ed962e00864ab84c95dd6252bedaee25c2
AJ <ajabad.dev@gmail.com>
2022-01-23 22:12:30 +0800

    Added route transition

--- src/assets/routetransitions.js
+++ src/assets/routetransitions.js
@@ -1,6 +1,7 @@
 const routeTransitions = new Map([
   ["VerifyEmail-WelcomeNewUser", "clip-push"],
   ["SignIn-WelcomeNewUser", "clip-push"],
+  ["WelcomeNewUser-BasicInfo", "clip-push"],
 ])
 
 export default routeTransitions
