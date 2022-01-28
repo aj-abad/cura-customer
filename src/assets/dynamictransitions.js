752a9d329584a658dff05e5fef7c7e0a58417c9d
AJ <ajabad.dev@gmail.com>
2022-01-28 14:23:25 +0800

    Made transition dynamic

--- src/assets/dynamictransitions.js
+++ src/assets/dynamictransitions.js
@@ -1,13 +1,9 @@
 
-const clipTransition = document.createElement("STYLE")
-clipTransition.setAttribute("data-clip-transition", "")
-//set to diagonal of screen plus 20px for padding
-const clipRadius = Math.pow((Math.pow(window.innerHeight), 2) + Math.pow(window.innerWidth, 2), 0.5) + 20
-clipTransition.innerHTML = `
-:root {
-  --clip-transition-radius: ${clipRadius}px;
-}
-`
-document.head.appendChild(clipTransition)
+const root = document.querySelector(":root")
 
-//TODO window resize event
+const setClipTransitionRadius = () => {
+  root.style.setProperty("--clip-transition-radius", `${Math.pow((Math.pow(window.innerHeight), 2) + Math.pow(window.innerWidth, 2), 0.5) + 20}px`);
+}
+
+setClipTransitionRadius()
+window.addEventListener("resize", setClipTransitionRadius)
