186747440faf9fa168fd0b09dddee45389a75ff1
AJ <ajabad.dev@gmail.com>
2022-01-28 15:16:05 +0800

    Added progress indicator component layout

--- src/components/Account/ProgressIndicator.vue
+++ src/components/Account/ProgressIndicator.vue
@@ -1,6 +1,18 @@
 <template>
-  <div>
+  <div
+    class="px-6 mb-6 d-flex justify-space-between"
+    :aria-label="`Step ${currentStep} of ${steps}`"
+  >
     <!-- TODO add progress indicator -->
+    <div
+      class="d-flex align-center"
+      v-for="i in steps"
+      :key="i"
+      :class="{ 'flex-grow-1': i < steps }"
+    >
+      <div class="step"></div>
+      <div class="line flex-grow-1" v-if="i < steps"></div>
+    </div>
   </div>
 </template>
 
@@ -9,8 +21,21 @@ export default {
   name: "ProgressIndicator",
   props: {
     steps: Number,
+    currentStep: Number,
   },
 };
 </script>
 
-<style lang="stylus" scoped></style>
+<style lang="stylus" scoped>
+.step {
+  height: 20px;
+  width: @height;
+  background: var(--v-bglight-darken2);
+  border-radius: 100%;
+}
+
+.line {
+  height: 4px;
+  background: var(--v-bglight-darken1);
+}
+</style>
