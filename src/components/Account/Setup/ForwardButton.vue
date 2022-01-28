cdd656aa9e573fdfc73d1b1881e7ca27b6ef913a
AJ <ajabad.dev@gmail.com>
2022-01-28 17:07:30 +0800

    Added forward button functionality

---
+++ src/components/Account/Setup/ForwardButton.vue
@@ -0,0 +1,21 @@
+<template>
+  <v-btn @click="goForward()" color="primary" elevation="0" large block>
+    Next <v-icon small>mdi-chevron-right</v-icon>
+  </v-btn>
+</template>
+<script>
+export default {
+  name: "ForwardButton",
+  props: {
+    currentStep: Number,
+  },
+  methods: {
+    goForward() {
+      const nextStep = this.$router.options.routes
+        .find((route) => route.name === "AccountSetup")
+        .children.find((route) => route.meta.step === this.currentStep + 1);
+      if (nextStep) return this.$router.push(nextStep.path);
+    },
+  },
+};
+</script>
