b152b23b413af9ed905b466881e6c2183bbb879c
AJ <ajabad.dev@gmail.com>
2022-01-28 12:15:29 +0800

    Changes to account setup page

--- src/views/Account/Setup/BasicInfo.vue
+++ src/views/Account/Setup/BasicInfo.vue
@@ -1,23 +1,40 @@
 <template>
   <div class="pa-6">
-    <h2>Basic information</h2>
-    <h3 class="font-weight-medium grey--text text--darken-2 mb-6">
-      Let's get to know you.
-    </h3>
+    <div class="mb-6">
+      <div class="d-flex align-center">
+        <h2>Basic information</h2>
+        <small class="ml-auto font-weight-semibold grey--text text--darken-2">
+          Step 1 of 3
+        </small>
+      </div>
+      <h3 class="font-weight-medium grey--text text--darken-2">
+        Let's get to know you.
+      </h3>
+    </div>
     <v-text-field
       dense
       rounded
       outlined
       hide-details
       label="First name"
-      class="mb-2"
+      class="mb-4"
+      v-model.trim="userInfo.firstName"
     />
-    <v-text-field dense rounded outlined hide-details label="Last name" />
+    <v-text-field
+      dense
+      rounded
+      outlined
+      hide-details
+      label="Last name"
+      v-model.trim="userInfo.lastName"
+    />
+    <h1 v-for="i in 100" :key="i">test</h1>
   </div>
 </template>
 
 <script>
 export default {
   name: "BasicInfo",
+  inject: ["userInfo"],
 };
 </script>
--- src/views/Account/Setup/Index.vue
+++ src/views/Account/Setup/Index.vue
@@ -3,9 +3,17 @@
     <div style="height: 96px">
       <!-- TODO add progress indicator -->
     </div>
-    <v-sheet class="rounded-xl flex-grow-1">
-      <router-view />
+    <v-sheet
+      class="rounded-xl bottom-sheet"
+      style="height: calc(100vh - 96px); overflow: hidden"
+    >
+      <router-view class="account-setup-view" />
     </v-sheet>
+    <div class="position-absolute w-100 pa-6 bottom left">
+      <v-btn color="primary" elevation="0" large block>
+        Next <v-icon right>mdi-chevron-right</v-icon>
+      </v-btn>
+    </div>
   </div>
 </template>
 
@@ -29,3 +37,11 @@ export default {
   },
 };
 </script>
+
+<style lang="stylus" scoped>
+.account-setup-view {
+  height: 100%;
+  overflow-y: auto;
+  padding-bottom: 64px !important;
+}
+</style>
