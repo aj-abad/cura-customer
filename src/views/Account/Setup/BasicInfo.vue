a7f96c35be950274b1af9265ed9aa74bde2c0816
AJ <ajabad.dev@gmail.com>
2022-02-03 19:19:38 +0800

    Changed basic account info page layout

--- src/views/Account/Setup/BasicInfo.vue
+++ src/views/Account/Setup/BasicInfo.vue
@@ -1,15 +1,13 @@
 <template>
   <div class="pa-6 h-100 w-100">
+    <v-dialog v-model="dialog">
+      <birth-date-picker @update="setBirthDate" />
+    </v-dialog>
     <div class="mb-6">
-      <div class="d-flex align-center">
-        <h2>Basic information</h2>
-        <small class="ml-auto font-weight-semibold grey--text text--darken-2">
-          Step {{ currentStep }} of {{ steps }}
-        </small>
-      </div>
-      <h3 class="font-weight-medium grey--text text--darken-2">
-        Let's get to know you.
-      </h3>
+      <small class="ml-auto font-weight-semibold grey--text text--darken-2">
+        Step {{ currentStep }} of {{ steps }}
+      </small>
+      <h2>Basic information</h2>
     </div>
     <v-text-field
       dense
@@ -19,6 +17,7 @@
       label="First name"
       class="mb-4"
       v-model.trim="userInfo.firstName"
+      id="first-name-input"
     />
     <v-text-field
       dense
@@ -27,13 +26,52 @@
       hide-details
       label="Last name"
       v-model.trim="userInfo.lastName"
+      class="mb-4"
+    />
+    <v-text-field
+      readonly
+      dense
+      rounded
+      outlined
+      hide-details
+      label="Birth date"
+      v-model.trim="formattedDate"
+      @focus.prevent="showDialog()"
     />
   </div>
 </template>
 
 <script>
+import focusElementOnMount from "@/mixins/focuselement";
+import BirthDatePicker from "@/components/Account/Setup/BirthDatePicker.vue";
 export default {
   name: "BasicInfo",
+  components: {
+    BirthDatePicker,
+  },
+  data() {
+    return {
+      focusElement: "#first-name-input",
+      dialog: false,
+      picker: null,
+    };
+  },
+  mixins: [focusElementOnMount],
+  methods: {
+    showDialog() {
+      document.activeElement.blur();
+      this.$nextTick(() => (this.dialog = true));
+    },
+    setBirthDate(date){
+      this.userInfo.birthDate = date;
+      this.dialog = false;
+    },
+  },
+  computed:{
+    formattedDate(){
+      return this.userInfo.birthDate ? this.$date(this.userInfo.birthDate).format("MMMM D, YYYY") : "";
+    }
+  },
   props: {
     steps: Number,
     currentStep: Number,
