4261d55712d135c0079f424ef9a6ac45e18d32c2
AJ <ajabad.dev@gmail.com>
2022-02-03 19:19:23 +0800

    Added date picker dialog for birth date

---
+++ src/components/Account/Setup/BirthDatePicker.vue
@@ -0,0 +1,27 @@
+<template>
+  <v-sheet class="rounded-lg overflow-hidden">
+    <v-date-picker :max="maximumDate" v-model="date" />
+    <div class="pa-4">
+      <v-btn block large elevation="0" color="primary" @click="$emit('update', date)"> Confirm </v-btn>
+    </div>
+  </v-sheet>
+</template>
+
+<script>
+export default {
+  name: "BirthDatePicker",
+  data() {
+    return {
+      date: null,
+    };
+  },
+  computed: {
+    maximumDate() {
+      return this.$date().subtract(18, "years").toISOString().substr(0, 10);
+    },
+  },
+  mounted() {
+    this.date = this.maximumDate;
+  },
+};
+</script>
