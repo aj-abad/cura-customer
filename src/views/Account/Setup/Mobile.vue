56ecf09a6554fa30e48c91ca59817284080cf6ab
AJ <ajabad.dev@gmail.com>
2022-02-04 18:07:29 +0800

    Changed mobile phone page layout

--- src/views/Account/Setup/Mobile.vue
+++ src/views/Account/Setup/Mobile.vue
@@ -1,29 +1,27 @@
 <template>
   <div class="pa-6 w-100 h-100 d-flex flex-column">
     <div class="mb-6">
-      <div class="d-flex align-center">
-        <h2>Mobile number</h2>
         <small class="ml-auto font-weight-semibold grey--text text--darken-2">
           Step {{ currentStep }} of {{ steps }}
         </small>
-      </div>
-      <h3 class="font-weight-medium grey--text text--darken-2">
-        We'll use this to contact you during bookings.
-      </h3>
+        <h2>Mobile number</h2>
     </div>
-    <v-text-field
-      dense
-      rounded
-      outlined
-      hide-details
-      label="Mobile number"
-      class="mb-4"
-      v-model.trim="mobile"
-      prefix="+63"
-      v-mask="'### ### ####'"
-    />
+    <div class="mb-4">
+      <v-text-field
+        dense
+        rounded
+        outlined
+        hide-details
+        label="Mobile number"
+        v-model.trim="mobile"
+        prefix="+63"
+        v-mask="'### ### ####'"
+      />
+    </div>
     <div class="mt-auto mb-2">
-      <v-btn plain elevation="0" large block> Maybe later </v-btn>
+      <v-btn @click="skipStep()" plain elevation="0" large block>
+        Maybe later
+      </v-btn>
     </div>
   </div>
 </template>
@@ -33,12 +31,22 @@ export default {
   name: "Mobile",
   data() {
     return {
-      mobile: "",
+      mobile: this.userInfo.mobile ?? "",
     };
   },
   props: {
     steps: Number,
     currentStep: Number,
+  },
+  methods: {
+    skipStep() {
+      const nextStep = this.$router.options.routes
+        .find((route) => route.name === "AccountSetup")
+        .children.find((route) => route.step === this.currentStep + 1);
+        
+      this.userInfo.mobile = "";
+      this.$router.push(nextStep);
+    },
   },
   watch: {
     mobile() {
