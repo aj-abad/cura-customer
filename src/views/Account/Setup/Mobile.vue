29acc35d9fcf663b75843df0aa568e80f9df45a3
AJ <ajabad.dev@gmail.com>
2022-02-07 17:58:49 +0800

    Added text in mobile step

--- src/views/Account/Setup/Mobile.vue
+++ src/views/Account/Setup/Mobile.vue
@@ -1,13 +1,14 @@
 <template>
   <div class="pa-6 w-100 h-100 d-flex flex-column">
     <div class="mb-6">
-        <small class="ml-auto font-weight-semibold grey--text text--darken-2">
-          Step {{ currentStep }} of {{ steps }}
-        </small>
-        <h2>Mobile number</h2>
+      <small class="ml-auto font-weight-semibold grey--text text--darken-2">
+        Step {{ currentStep }} of {{ steps }}
+      </small>
+      <h2>Mobile number</h2>
     </div>
-    <div class="mb-4">
+    <div>
       <v-text-field
+        class="mb-4"
         dense
         rounded
         outlined
@@ -17,8 +18,14 @@
         prefix="+63"
         inputmode="numeric"
         v-mask="'### ### ####'"
-          pattern="[0-9]{1}"
+        pattern="[0-9]{1}"
       />
+      <p>
+        <small>
+          We'll use your mobile number for two-factor authentication (2FA) and to
+          contact you during bookings.
+        </small>
+      </p>
     </div>
     <div class="mt-auto mb-2">
       <v-btn @click="skipStep()" plain elevation="0" large block>
@@ -29,8 +36,8 @@
 </template>
 
 <script>
-import multiStep from "@/mixins/multistepform"
-import {validateMobile} from "cura-validation-utils"
+import multiStep from "@/mixins/multistepform";
+import { validateMobile } from "cura-validation-utils";
 export default {
   name: "Mobile",
   data() {
@@ -47,15 +54,15 @@ export default {
       const nextStep = this.$router.options.routes
         .find((route) => route.name === "AccountSetup")
         .children.find((route) => route.meta.step === this.currentStep + 1);
-        
+
       this.userInfo.mobile = "";
       this.$router.push(nextStep);
     },
   },
-  computed:{
-    canGoForward(){
+  computed: {
+    canGoForward() {
       return validateMobile(this.userInfo.mobile);
-    }
+    },
   },
   watch: {
     mobile() {
