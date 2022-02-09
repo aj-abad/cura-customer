e9ae5de3dec52be2a3991304b87bdf176cbe0ca6
AJ <ajabad.dev@gmail.com>
2022-02-09 15:01:08 +0800

    Minor changes

--- src/views/Account/Setup/Mobile.vue
+++ src/views/Account/Setup/Mobile.vue
@@ -1,32 +1,39 @@
 <template>
   <div class="pa-6 w-100 h-100 d-flex flex-column">
+    <v-dialog v-model="meta.mobileDialog">
+      <verify-mobile :mobile="mobile" />
+    </v-dialog>
     <div class="mb-6">
       <small class="ml-auto font-weight-semibold grey--text text--darken-2">
         Step {{ currentStep }} of {{ steps }}
       </small>
       <h2>Mobile number</h2>
     </div>
-    <div>
+    <form @submit.prevent="$emit('forward')">
       <v-text-field
         class="mb-4"
         dense
         rounded
         outlined
-        hide-details
+        :errorMessages="mobileErrorMessages"
+        hide-details="auto"
         label="Mobile number"
         v-model.trim="mobile"
         prefix="+63"
         inputmode="numeric"
         v-mask="'### ### ####'"
-        pattern="[0-9]{1}"
+        v-focus="320"
+        :readonly="!!userInfo.mobile"
+        :append-icon="!!userInfo.mobile ? 'mdi-check' : null"
       />
       <p>
         <small>
-          We'll use your mobile number for two-factor authentication (2FA) and to
-          contact you during bookings.
+          We'll use your mobile number for two-factor authentication (2FA) and
+          to contact you during bookings.
         </small>
       </p>
-    </div>
+      <button type="submit" class="d-none"></button>
+    </form>
     <div class="mt-auto mb-2">
       <v-btn @click="skipStep()" plain elevation="0" large block>
         Maybe later
@@ -38,8 +45,12 @@
 <script>
 import multiStep from "@/mixins/multistepform";
 import { validateMobile } from "cura-validation-utils";
+import VerifyMobile from "@/components/Account/Setup/VerifyMobile";
 export default {
   name: "Mobile",
+  components: {
+    VerifyMobile,
+  },
   data() {
     return {
       mobile: this.userInfo.mobile ?? "",
@@ -60,16 +71,26 @@ export default {
     },
   },
   computed: {
-    canGoForward() {
-      return validateMobile(this.userInfo.mobile);
+    mobileUnmask() {
+      return this.mobile.replaceAll(" ", "");
     },
-  },
-  watch: {
-    mobile() {
-      return (this.userInfo.mobile = this.mobile.replaceAll(" ", ""));
+    canGoForward() {
+      return validateMobile(this.mobileUnmask);
+    },
+    mobileErrorMessages() {
+      if (this.mobileUnmask.length === 10 && !validateMobile(this.mobileUnmask))
+        return ["Please enter a valid mobile number."];
+      return null;
     },
   },
   inject: ["userInfo", "meta"],
   mixins: [multiStep],
+  beforeRouteLeave(to, from, next) {
+    if (this.meta.mobileDialog) {
+      this.meta.mobileDialog = false;
+      return next(false);
+    }
+    return next();
+  },
 };
 </script>
