891b9071badc1749ecef5cd6eed91bf4ef6af8dd
AJ <ajabad.dev@gmail.com>
2022-02-10 17:38:19 +0800

    Added profile photo and name preview in confirmation page

--- src/views/Account/Setup/Confirmation.vue
+++ src/views/Account/Setup/Confirmation.vue
@@ -1,11 +1,35 @@
 <template>
-  <div>
-
+  <div class="pa-6 w-100 h-100 d-flex flex-column">
+    <div class="mb-n4">
+      <small class="ml-auto font-weight-semibold grey--text text--darken-2">
+        Step {{ currentStep }} of {{ steps }}
+      </small>
+      <h2>Confirmation</h2>
+    </div>
+    <div class="profile-photo-btn-container pa-12">
+      <div class="h-100 w-100 position-relative">
+        <profile-photo-preview :profilePhotoPreview="userInfo.profilePhoto" />
+      </div>
+    </div>
+    <div class="text-center mt-n8">
+      <h3>
+        {{ userInfo.firstName }} {{userInfo.lastName}}
+      </h3>
+    </div>
   </div>
 </template>
 
 <script>
+import ProfilePhotoPreview from "@/components/Account/Setup/ProfilePhotoPreview";
 export default {
-  name: "Confirmation"
-}
+  name: "Confirmation",
+  components: {
+    ProfilePhotoPreview,
+  },
+  props: {
+    steps: Number,
+    currentStep: Number,
+  },
+  inject: ["userInfo"],
+};
 </script>
