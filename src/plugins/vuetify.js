4465a58431ac494d8ffa4bca6e3d94500175c518
AJ <ajabad.dev@gmail.com>
2022-02-10 13:52:40 +0800

    Changed vuetify settings

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -1,6 +1,6 @@
-import Vue from 'vue';
-import Vuetify from 'vuetify/lib/framework';
-import '@mdi/font/css/materialdesignicons.css'
+import Vue from "vue";
+import Vuetify from "vuetify/lib/framework";
+import "@mdi/font/css/materialdesignicons.css";
 import "@fontsource/nunito-sans/200.css";
 import "@fontsource/nunito-sans/300.css";
 import "@fontsource/nunito-sans/400.css";
@@ -10,24 +10,28 @@ import "@fontsource/nunito-sans/800.css";
 import "@fontsource/nunito-sans/900.css";
 Vue.use(Vuetify);
 
-export default new Vuetify({
+const opts = {
   theme: {
+    // dark: true,
     options: {
       customProperties: true,
     },
-    themes: {
-      light: {
-        primary: '#0d826e',
-        primarylight: '#00a987',
-        primarylighter: '#aeddd5',
-        secondary: '#f69b7a',
-        secondarylight: '#f7eab5',
-        error: '#820D21',
-        bglight: '#dff2ef'
-        // secondary: '#b0bec5',
-        // accent: '#8c9eff',
-        // error: '#b71c1c',
-      },
+  },
+  themes: {
+    light: {
+      background: "#fff",
+      primary: "#0d826e",
+      primarylight: "#00a987",
+      primarylighter: "#aeddd5",
+      secondary: "#f69b7a",
+      secondarylight: "#f7eab5",
+      error: "#820D21",
+      bglight: "#dff2ef",
+    },
+    dark: {
+      background: "#1e1e1e",
     },
   },
-});
+};
+
+export default new Vuetify(opts);
