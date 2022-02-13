9518a64261ad4af14b7170f1ad06a1ae9f80a9c3
AJ <ajabad.dev@gmail.com>
2022-02-13 19:16:51 +0800

    Made app name not static

--- src/plugins/axios.js
+++ src/plugins/axios.js
@@ -3,5 +3,5 @@ import Axios from 'axios'
 import {version} from '../../package.json'
 Axios.defaults.baseURL = process.env.VUE_APP_API
 Axios.defaults.headers.post['Content-Type'] = 'application/json'
-Axios.defaults.headers.common['App'] = `Cura Customer v${version}`
+Axios.defaults.headers.common['App'] = `${process.env.VUE_APP_NAME} - v${version}`
 Vue.prototype.$http = Axios
