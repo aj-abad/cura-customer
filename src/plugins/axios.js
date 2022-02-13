b355c4aa3888d4ada96f93ca75a12e2a164ede81
AJ <ajabad.dev@gmail.com>
2022-02-13 18:34:35 +0800

    Added app version header

--- src/plugins/axios.js
+++ src/plugins/axios.js
@@ -1,7 +1,7 @@
 import Vue from 'vue'
 import Axios from 'axios'
+import {version} from '../../package.json'
 Axios.defaults.baseURL = process.env.VUE_APP_API
-// Axios.defaults.baseURL = "/api";
 Axios.defaults.headers.post['Content-Type'] = 'application/json'
-
+Axios.defaults.headers.common['App'] = `Cura Customer v${version}`
 Vue.prototype.$http = Axios
