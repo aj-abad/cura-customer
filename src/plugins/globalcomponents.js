1b50005f7c946487e35ed4898ba0ae598266ef48
AJ <ajabad.dev@gmail.com>
2022-03-06 18:25:02 +0800

    Globally registered labeled text field component

--- src/plugins/globalcomponents.js
+++ src/plugins/globalcomponents.js
@@ -1,3 +1,5 @@
 import Vue from 'vue'
 import BackNav from '../components/Shared/BackNav'
+import LabeledTextField from "../components/Shared/LabeledTextField";
 Vue.component('back-nav', BackNav)
+Vue.component("v-labeled-text-field", LabeledTextField);
