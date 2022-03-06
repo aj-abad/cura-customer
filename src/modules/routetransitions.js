269ae3c42df8d967cb796d4d858c58935bf1cf3a
AJ <ajabad.dev@gmail.com>
2022-03-06 18:27:42 +0800

    Added custom transition from start to email

--- src/modules/routetransitions.js
+++ src/modules/routetransitions.js
@@ -1,4 +1,6 @@
 const routeTransitions = new Map([
+  ["Start-Email", "slide-parallax-push"],
+  ["Email-Start", "slide-parallax-pop"],
   ["VerifyEmail-WelcomeNewUser", "clip-push"],
   ["SignIn-WelcomeNewUser", "clip-push"],
   ["WelcomeNewUser-BasicInfo", "slide-parallax-push"],
