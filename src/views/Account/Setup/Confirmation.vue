3f53e60e1f8657c69bdcbbb408fe1f6d75b141da
AJ <ajabad.dev@gmail.com>
2022-03-07 20:41:26 +0800

    Made some changes to final account setup screen

--- src/views/Account/Setup/Confirmation.vue
+++ src/views/Account/Setup/Confirmation.vue
@@ -1,23 +1,28 @@
 <template>
   <div class="pa-6 w-100 h-100 d-flex flex-column">
-    <div class="mb-n4">
+    <div class="mb-4">
       <small class="ml-auto font-weight-semibold grey--text text--darken-2">
         Step {{ currentStep }} of {{ steps }}
       </small>
       <h2>Confirmation</h2>
     </div>
-    <div class="profile-photo-btn-container pa-12">
-      <div class="h-100 w-100 position-relative">
-        <profile-photo-preview v-if="userInfo.profilePhoto.url" :profilePhotoPreview="userInfo.profilePhoto" />
-        <!-- TODO add meaningful profile photo placeholder -->
-        <div class="h-100 w-100 red" style="border-radius: 1000px" v-else ></div>
+    <v-sheet class="mx-6 pa-6 rounded-lg">
+      <div class="profile-photo-btn-container pa-6 mt-n6 mb-n4">
+        <div class="h-100 w-100 position-relative">
+          <profile-photo-preview v-if="userInfo.profilePhoto.url" :profilePhotoPreview="userInfo.profilePhoto" />
+          <!-- TODO add meaningful profile photo placeholder -->
+          <div class="h-100 w-100 red" style="border-radius: 1000px" v-else ></div>
+        </div>
       </div>
-    </div>
-    <div class="text-center mt-n8">
-      <h3>
-        {{ userInfo.firstName }} {{userInfo.lastName}}
-      </h3>
-    </div>
+      <div class="text-center">
+        <h3>
+          {{ userInfo.firstName }} {{userInfo.lastName}}
+        </h3>
+        <h4 v-if="userInfo.mobile" class="grey--text text--darken-2 font-weight-medium">
+          +63{{ userInfo.mobile }}
+        </h4>
+      </div>
+    </v-sheet>
   </div>
 </template>
 
